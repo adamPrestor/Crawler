e6d5a633da6b667d51d23b738ee92c91c19c7743
Lojze Žust <lojze.zust@gmail.com>
2020-03-31 18:35:10 +0200

    Num workers argument

--- Crawler.py
+++ Crawler.py
@@ -1,3 +1,4 @@
+import argparse
 import logging
 import urllib
 import urllib.request
@@ -11,7 +12,7 @@ from worker import URLWorker
 import settings
 
 
-def crawl():
+def crawl(num_workers):
     lock = Lock()
 
     db.init_frontier()
@@ -20,11 +21,17 @@ def crawl():
     for url in settings.INIT_FRONTIER:
         db.add_to_frontier(url)
 
-    for i in range(settings.DEFAULT_NUM_WORKERS):
+    for i in range(num_workers):
         URLWorker(lock, i).start()
 
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Configurable multithreaded web crawler.")
+    parser.add_argument('--num_workers', '-n', type=int, default=settings.DEFAULT_NUM_WORKERS,
+                        help=f'Number of parallel crawlers. Defaults to {settings.DEFAULT_NUM_WORKERS}')
+
+    args = parser.parse_args()
+
     logging.basicConfig(filename='crawler.log', level=logging.NOTSET, format='%(levelname)s:%(asctime)s: %(message)s')
     logging.critical("Crawler started.")
-    crawl()
+    crawl(args.num_workers)

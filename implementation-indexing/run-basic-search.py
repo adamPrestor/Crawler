721b2beaa536d59eecdbddd49aee37d409d210d4
adam prestor <adam.prestor@gmail.com>
2020-05-22 22:13:29 +0200

    Add scripts that can be run from console

---
+++ implementation-indexing/run-basic-search.py
@@ -0,0 +1,6 @@
+import sys
+import htmlsearch
+
+
+if __name__ == '__main__':
+    print(htmlsearch.main(sys.argv[1], 6))
---
+++ implementation-indexing/run-sql-search.py
@@ -0,0 +1,6 @@
+import sys
+import sqlsearch
+
+
+if __name__ == '__main__':
+    print(sqlsearch.main(sys.argv[1], 6))
